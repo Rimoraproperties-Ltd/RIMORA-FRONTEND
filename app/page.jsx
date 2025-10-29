24e07aa36b0512aefc41a4796a41d400bbedbdc7
Rimoraproperties-Ltd <Rimoraproperties@gmail.com>
2025-10-29 14:55:04 +0100

    Delete app directory

--- app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Welcome to Rimora Properties</h1>
-      <p>Your future home awaits. Coming soon...</p>
-    </main>
-  );
-}

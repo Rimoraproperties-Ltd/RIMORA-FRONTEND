7de372b550dd0379a92dfb36fdc56a67dd183daa
Rimoraproperties-Ltd <Rimoraproperties@gmail.com>
2025-10-29 14:48:40 +0100

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to Rimora Properties</h1>
+      <p>Your future home awaits. Coming soon...</p>
+    </main>
+  );
+}
